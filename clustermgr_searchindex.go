70f305b37edc6c2442fdc72203c217e20a52c029
PumpkinSeed <qwer.kocka@gmail.com>
2023-02-27 17:54:31 +0100

    Initial commit

---
+++ clustermgr_searchindex.go
@@ -0,0 +1,106 @@
+package gocbm
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"sync"
+	"time"
+
+	"github.com/couchbase/gocbcore/v9"
+)
+
+type GetIndexStatsOpts struct {
+	MemdAddrs []string
+	HTTPAddrs []string
+	Username  string
+	Password  string
+
+	AgentGroupConfig *gocbcore.AgentGroupConfig
+
+	IndexName string
+}
+
+type IndexStatsResponse struct {
+	PIndexes map[string]PIndex `json:"pindexes"`
+}
+
+type PIndex struct {
+	Basic PIndexBasic `json:"basic"`
+}
+
+type PIndexBasic struct {
+	DocCount int64 `json:"DocCount"`
+}
+
+func GetIndexStats(opts GetIndexStatsOpts) (*IndexStatsResponse, error) {
+	agentGroup, err := gocbcore.CreateAgentGroup(opts.GetAgentGroupConfig())
+	if err != nil {
+		return nil, err
+	}
+	coreReq := &gocbcore.HTTPRequest{
+		Service: gocbcore.FtsService,
+		Method:  "GET",
+		Path:    fmt.Sprintf("/api/stats/index/%s", opts.IndexName),
+	}
+
+	var wg = &sync.WaitGroup{}
+	var innerErr error
+	var innerData []byte
+
+	wg.Add(1)
+	_, err = agentGroup.DoHTTPRequest(coreReq, func(response *gocbcore.HTTPResponse, err error) {
+		defer wg.Done()
+		if err != nil {
+			innerErr = err
+		}
+		data, err := io.ReadAll(response.Body)
+		if err != nil {
+			innerErr = err
+		}
+		innerData = data
+	})
+	if err != nil {
+		return nil, err
+	}
+	wg.Wait()
+	if innerErr != nil {
+		return nil, innerErr
+	}
+
+	var resp IndexStatsResponse
+	err = json.Unmarshal(innerData, &resp)
+	if err != nil {
+		return nil, err
+	}
+
+	return &resp, nil
+}
+
+func (opts GetIndexStatsOpts) GetAgentGroupConfig() *gocbcore.AgentGroupConfig {
+	if opts.AgentGroupConfig != nil {
+		return opts.AgentGroupConfig
+	}
+
+	return &gocbcore.AgentGroupConfig{
+		AgentConfig: gocbcore.AgentConfig{
+			MemdAddrs: opts.MemdAddrs,
+			HTTPAddrs: opts.HTTPAddrs,
+			UserAgent: "gocb/v2.2.4", // I don't care
+			Auth: gocbcore.PasswordAuthProvider{
+				Username: opts.Username,
+				Password: opts.Password,
+			},
+			UseMutationTokens:      true,
+			UseDurations:           true,
+			UseOutOfOrderResponses: true,
+			UseCollections:         true,
+			ConnectTimeout:         10000000000,
+			KVConnectTimeout:       7 * time.Second,
+			NoRootTraceSpans:       true,
+			DefaultRetryStrategy:   nil,
+			CircuitBreakerConfig:   gocbcore.CircuitBreakerConfig{},
+			UseZombieLogger:        true,
+		},
+	}
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/infiniteloopcloud/gocbm
+
+go 1.18
+
+require (
+	github.com/couchbase/gocbcore/v9 v9.1.8 // indirect
+	github.com/golang/snappy v0.0.1 // indirect
+	github.com/google/uuid v1.1.1 // indirect
+)
